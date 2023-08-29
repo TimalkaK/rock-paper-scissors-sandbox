5ce68ae40b666ac05799ddfbf2b7ee74a502b7b4
TimalkaK <timalkamk@gmail.com>
2023-08-29 17:48:55 +0100

    feat: Implemented updateDOM function

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ const getRandomMove = () => {
   const updateDOM = (moveOne, moveTwo, outcome) => {
     // TODO Implement this method to update the DOM
     // There are some images you can use in the images directory
+    console.log(outcome);
+    document.getElementById('player-one-move__img').src = `images/${moveOne}.png`;
+    document.getElementById('player-two-move__img').src = `images/${moveTwo}.png`;
+    document.getElementById("player-one-move__name").textContent = moveOne;
+    document.getElementById("player-two-move__name").textContent = moveTwo;
+    
+    document.getElementById("winner-outcome").textContent = outcome;
   };
   
   const playButton = document.getElementById("play-btn");
